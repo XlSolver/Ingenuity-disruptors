77c0f0ebb89cd69540fe9c928abd01792d01d519
FrancescaFerrini <75753679+FrancescaFerrini@users.noreply.github.com>
2024-02-22 14:06:05 +0100

    Add files via upload

---
+++ BarcodeScanner1/BarcodeScanner1/BarcodeScanner1App.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScanner1App.swift
+//  BarcodeScanner1
+//
+//  Created by Francesca Ferrini on 20/02/24.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScanner1App: App {
+    var body: some Scene {
+        WindowGroup {
+            TabController()
+        }
+    }
+}
